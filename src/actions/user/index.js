9771afd13c6027595aca700bfca9bcee854f5062
wblazejewski <firethar@gmail.com>
2018-06-24 11:05:41 +0200

    update

--- src/actions/user/index.js
+++ src/actions/user/index.js
@@ -1,3 +1,3 @@
 export const LOGIN_START = "LOGIN_START";
 
-export const startLogin = (login, password) => ({ type: LOGIN_START, login, password})
+export const startLogin = (login, password) => ({ type: LOGIN_START, login, password});
